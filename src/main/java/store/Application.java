8c4e7653d5a25fb6b670cb0baf33d9d36301b4c2
yebrong <officeguider@naver.com>
2024-11-11 22:37:34 +0900

    feat : Application

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,10 @@
 package store;
 
+import store.mediator.StoreMediator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final StoreMediator storeMediator = new StoreMediator();
+        storeMediator.run();
     }
 }
